42a5b99040fd44f441e32d7079556a5514c08991
Kotocade <almonds@mit.edu>
2023-03-03 20:19:43 -0500

    Add starter mail_scripts

---
+++ mail_scripts/send_to_backend.py
@@ -0,0 +1,20 @@
+"""
+Forward the email to backend at the correct endpoint
+"""
+
+import sys
+import os
+import requests
+
+ENDPOINT = f""
+
+def save_last_email(email, append=false):
+  mode = "a" if append else "w"
+  with open("last.txt", mode) as f:
+    f.write(email)
+
+if __name__ == "__main__":
+  email = sys.stdin.read()
+  save_last_email(email)
+
+
