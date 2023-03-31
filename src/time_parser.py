0936dbeaa36558af0ecb0ea3ce79037f4ba194c1
Kotocade <almonds@mit.edu>
2023-03-31 16:40:19 -0400

    Enhance time_parser
    * smarter setup/type annotations
    * passes most tests, but requires attention for "1-4pm"
    * could use some better organization

--- src/time_parser.py
+++ src/time_parser.py
@@ -1,49 +1,83 @@
+from __future__ import annotations
+from dataclasses import dataclass
+from typing import (
+    Callable, Tuple, List, Dict, Any,
+    Optional, Union, Final, Generic, TypeVar,
+)
+A = TypeVar("A")
+B = TypeVar("B")
+
 import re
 import datetime
 
-from dataclasses import dataclass
-from typing import Callable, Final, Tuple, List, Dict, Optional
-
 @dataclass
-class EventTime:
-    start: Tuple[Optional[datetime.date], Optional[datetime.time]]
-    end: Tuple[Optional[datetime.date], Optional[datetime.time]]
+class Parser(Generic[A]):
+    """Represents a regex parser to extract info from text
 
-@dataclass
-class Pattern:
-    """Represents a pattern to look for
+    The assembled ``Parser[NamedTuple]`` object can be called on a string
+    (``text``) to attempt the parsing. If successful, it returns the parsed
+    data as the ``NamedTuple`` type. Otherwise returns `None`.
 
     Attributes:
-        pattern: The regex pattern to search for
-        groups: The respective names corresponding to the regex pattern groups
-        checks: Any additional checks to perform for respective args
+        output: The type of the parsed output, if successful.
+        pattern: The regex pattern to search for. The respective regex named
+            groups correspond exactly to the ``NamedTuple`` fields.
+        subparsers: The respective functions or parsers to call on for the
+            extracted group text.
+        tweak: Any additional finishing touches to perform on the returned
+            info.
     """
+    output: type
     pattern: str
-    groups: Tuple[str,...]
-    checks: Dict[str, Callable[[str], bool]]
+    subparsers: List[Union[Parser, Callable[[str], Any]]]
+    tweak: Callable[[A], Optional[A]] = lambda parsed: parsed
 
-    def __call__(self, text: str) -> Optional[Dict[str, str]]:
+    def __post_init__(self) -> None:
+        if hasattr(self.output, "__annotations__"):
+            self.annotations = self.output.__annotations__
+        else:
+            self.annotations = self.output.__origin__.__annotations__
+        assert self.annotations, \
+            f"original output type {self.output!r} must have type annotations"
+
+    def __call__(self, text: str) -> Optional[A]:
         for match in re.finditer(self.pattern, text, flags=re.IGNORECASE):
-            if not match: continue
+            groups = match.groupdict()
             kwargs = {}
-            for group_name, group in zip(self.groups, match.groups()):
-                if group is not None:
-                    kwargs[group_name] = group
-
-            failed_checks = False
-            for key, check in self.checks.items():
-                if not check(kwargs[key]):
-                    failed_checks = True
+            successfully_parsed = True
+            for i, name in enumerate(self.annotations):
+                value = self.subparsers[i](groups[name])
+                if value is None:
+                    successfully_parsed = False
                     break
+                kwargs[name] = value
+            if not successfully_parsed: continue
 
-            if failed_checks: continue
+            parsed = self.output(**kwargs)
+            parsed = self.tweak(parsed)
+            if parsed is not None:
+                return parsed
 
-            return kwargs
+        return None
 
+    def __str__(self):
+        return self.pattern
+
+@dataclass
+class ParserChain(Generic[A, B]):
+    parsers: List[Parser]
+    formatter: Callable[[A], B]
+
+    def __call__(self, text: str, **kwargs) -> Optional[B]:
+        for parser in self.parsers:
+            parsed = parser(text)
+            if parsed is not None:
+                return self.formatter(parsed, **kwargs)
         return None
 
 TODAY = datetime.date.today()
 
+# non-numerical names for months or hours
 MONTHS: Final[Dict[str, int]] = {
     "January":   1,     "Jan": 1,
     "February":  2,     "Feb": 2,
@@ -58,80 +92,223 @@ MONTHS: Final[Dict[str, int]] = {
     "November":  11,    "Nov": 11,
     "December":  12,    "Dec": 12,
 }
+HOURS: Final[Dict[str, int]] = {
+    "Noon": 12,
+    "Midnight": 0,
+}
 
-DATE_PATTERNS: Final[List[Pattern]] = [
-    Pattern(r"(\w+)\s+(\d{1,2})",    ("month_name", "day"), {"month_name": lambda x: x.capitalize() in MONTHS}),
-    Pattern(r"(\d{1,2})\s+(\w+)",    ("day", "month_name"), {"month_name": lambda x: x.capitalize() in MONTHS}),
-    Pattern(r"(\d{1,2})\/(\d{1,2})", ("month", "day"),      {}),
-]
-TIME_RANGE_PATTERN: Final[str] = r"\b(\S+)\b\s*(?:[-–]|to|until)\s*\b(\S+)\b"
-TIME_PATTERNS: Final[List[Pattern]] = [
-    Pattern(r"(\d{1,2})(?::(\d{2}))?\s*((?:a|p)\.?m?\.?)?\b", ("hour", "minute", "am_pm"), {}),
-    Pattern(r"\b(noon|midnight)\b", ("special",), {}),
-]
-SPECIAL_TIMES = {"noon": 12, "midnight": 0}
+@dataclass
+class MonthNameDay:
+    month_name: str
+    day: int
 
-def format_time(**kwargs: str) -> datetime.time:
-    """Format a time given some combination of arguments
+@dataclass
+class MonthDay:
+    month: int
+    day: int
 
-    Keyword args:
-        hour: Optional.
-        minute: Optional.
-        am_pm: "am" or "pm". Optional.
+Date = Union[MonthNameDay, MonthDay]
 
-    Raises:
-        ValueError: If a respective arg cannot be converted to an int.
-        TypeError: If neither `hour` nor `special` is supplied
-    """
-    if "special" in kwargs:
-        h = SPECIAL_TIMES[kwargs["special"].lower()]
-    elif "hour" in kwargs:
-        h = int(kwargs["hour"])
-    else:
-        raise TypeError("either 'hour' or 'special' must be supplied")
+_parser_month_name_day = Parser[MonthNameDay](
+    MonthNameDay,
+    r"(?P<month_name>\w+)\s+(?P<day>\d{1,2})",
+    [str, int],
+    lambda parsed: parsed if parsed.month_name.capitalize() in MONTHS else None,
+)
+_parser_day_month_name = Parser[MonthNameDay](
+    MonthNameDay,
+    r"(?P<day>\d{1,2})\s+(?P<month_name>\w+)",
+    [str, int],
+    lambda parsed: parsed if parsed.month_name.capitalize() in MONTHS else None,
+)
+_parser_month_day = Parser[MonthDay](
+    MonthDay,
+    r"(?P<month>\d{1,2})\/(?P<day>\d{1,2})",
+    [int, int],
+)
 
-    m = int(kwargs["minute"]) if "minute" in kwargs else 0
-    am_pm = kwargs.get("am_pm", "").lower().replace(".", "")
+def _format_month_name_day(parsed: MonthNameDay, *, today: datetime.date=TODAY) -> datetime.date:
+    return datetime.date(
+        today.year,
+        MONTHS[parsed.month_name.capitalize()],
+        parsed.day,
+    )
 
-    if am_pm in ("pm", "p") and h != 12:
+def _format_month_day(parsed: MonthDay, *, today: datetime.date=TODAY) -> datetime.date:
+    return datetime.date(today.year, parsed.month, parsed.day)
+
+_date_formatters = {
+    MonthNameDay: _format_month_name_day,
+    MonthDay: _format_month_day,
+}
+
+def format_date(parsed: Date, *, today: datetime.date=TODAY) -> datetime.date:
+    date = _date_formatters[type(parsed)](parsed, today=today)
+    if date < today: date = date.replace(year=date.year+1)
+    return date
+
+DATE_PARSER_CHAIN = ParserChain[Date, datetime.date](
+    [
+        _parser_month_name_day,
+        _parser_day_month_name,
+        _parser_month_day,
+    ],
+    format_date,
+)
+
+
+@dataclass
+class HourOnly:
+    hour: int
+
+@dataclass
+class HourMinute:
+    hour: int
+    minute: int
+
+@dataclass
+class HourMinutePeriod:
+    hour: int
+    minute: int
+    period: str
+
+@dataclass
+class HourName:
+    hour_name: str
+
+Time = Union[HourOnly, HourMinute, HourMinutePeriod, HourName]
+
+@dataclass
+class TimeRange(Generic[A, B]):
+    start: A
+    end: B
+
+HH = r"(?P<hour>\d{1,2})"                # h or hh
+MM = r"(?::(?P<minute>\d{2}))"           # :mm
+PERIOD = r"(?:(?P<period>a|p)\.?m?\.?)" # a, am, a.m.
+
+_parser_hour_only = Parser[HourOnly](HourOnly, fr"\b{HH}\b", [int])
+_parser_hour_period = Parser[HourMinutePeriod](
+    HourMinutePeriod,
+    fr"\b{HH}{MM}?\s*{PERIOD}\b",
+    [int, lambda mm: 0 if mm is None else int(mm), str],
+)
+_parser_hour_minute = Parser[HourMinutePeriod](
+    HourMinutePeriod,
+    fr"\b{HH}{MM}\s*{PERIOD}?\b",
+    [int, int, lambda period: "a" if period is None else str(period)],
+)
+_parser_hour_name = Parser[HourName](
+    HourName,
+    r"\b(?P<hour_name>noon|midnight)\b",
+    [lambda name: name.capitalize()],
+)
+
+def _format_hour_only(parsed: HourOnly) -> datetime.time:
+    return datetime.time(parsed.hour)
+
+def _format_hour_minute(parsed: HourMinute) -> datetime.time:
+    return datetime.time(parsed.hour, parsed.minute)
+
+def _format_hour_minute_period(parsed: HourMinutePeriod) -> datetime.time:
+    h = parsed.hour
+    m = parsed.minute if parsed.minute is not None else 0
+
+    if parsed.period is None:
+        return datetime.time(h, m)
+
+    period = parsed.period.lower()
+    if period == "p" and h < 12:
         h += 12
-    elif am_pm in ("am", "a") and h == 12:
+    elif period == "a" and h == 12:
         h = 0
 
     return datetime.time(h, m)
 
-def format_date(*, day: str, today=TODAY, **kwargs: str) -> datetime.date:
-    """Format a date given some combination of arguments
+def _format_hour_name(parsed: HourName) -> datetime.time:
+    return datetime.time(HOURS[parsed.hour_name])
 
-    Keyword args:
-        month: Month number, where 1 represents January. Required if `month_name` is not supplied.
-        month_name: e.g. "January". Required if `month` is not supplied.
+_time_formatters = {
+    HourOnly: _format_hour_only,
+    HourMinute: _format_hour_minute,
+    HourMinutePeriod: _format_hour_minute_period,
+    HourName: _format_hour_name,
+}
 
-    Raises:
-        ValueError: If a respective arg cannot be converted to an int.
-        TypeError: If neither `month` nor `month_name` is supplied
-    """
-    if "month" in kwargs:
-        m = int(kwargs["month"])
-    elif "month_name" in kwargs:
-        month_name = kwargs["month_name"]
-        m = MONTHS.get(month_name.capitalize())
-        if m is None:
-            raise ValueError(f"invalid month name: {month_name!r}")
-    else:
-        raise TypeError("either 'month' or 'month_name' must be supplied")
+def format_time(parsed: Time) -> datetime.time:
+    return _time_formatters[type(parsed)](parsed)
 
-    d = int(day)
-    
-    # if the parsed date has already passed in the current year,
-    # then assume it's in the next calendar year
-    y = today.year
-    parsed_date = datetime.date(y, m, d)
-    if parsed_date < today: y += 1
-        
-    return datetime.date(y, m, d)
+TIME_PARSER_CHAIN = ParserChain(
+    [
+        _parser_hour_period,
+        _parser_hour_minute,
+        _parser_hour_name,
+    ],
+    format_time,
+)
 
-def parse_event_time(text: str, today: datetime.date=TODAY) -> EventTime:
+TIME_RANGE_PATTERN = r"\b(?P<start>\S+)\b\s*(?:[-–]|to|until)\s*\b(?P<end>\S+)\b"
+
+def format_time_range(parsed: TimeRange) -> Tuple[datetime.time, datetime.time]:
+    return (
+        format_time(parsed.start),
+        format_time(parsed.end),
+    )
+
+TIME_RANGE_PARSER_CHAIN = ParserChain[TimeRange, Tuple[datetime.time, datetime.time]](
+    [
+        Parser[TimeRange[HourOnly, HourMinutePeriod]]( # 10-10:30
+            TimeRange[HourOnly, HourMinutePeriod],
+            TIME_RANGE_PATTERN,
+            [_parser_hour_only, _parser_hour_minute],
+        ),
+        Parser[TimeRange[HourMinutePeriod, HourOnly]]( # 10:30-11
+            TimeRange[HourMinutePeriod, HourOnly],
+            TIME_RANGE_PATTERN,
+            [_parser_hour_minute, _parser_hour_only],
+        ),
+        Parser[TimeRange[HourMinutePeriod, HourMinutePeriod]]( # 10:00-11:00
+            TimeRange[HourMinutePeriod, HourMinutePeriod],
+            TIME_RANGE_PATTERN,
+            [_parser_hour_minute, _parser_hour_minute],
+        ),
+        Parser[TimeRange[HourOnly, HourMinutePeriod]]( # 10-11am
+            TimeRange[HourOnly, HourMinutePeriod],
+            TIME_RANGE_PATTERN,
+            [_parser_hour_only, _parser_hour_period],
+        ),
+        Parser[TimeRange[HourMinutePeriod, HourMinutePeriod]]( # 10am-11am
+            TimeRange[HourMinutePeriod, HourMinutePeriod],
+            TIME_RANGE_PATTERN,
+            [_parser_hour_period, _parser_hour_period],
+        ),
+        Parser[TimeRange[HourMinutePeriod, HourMinutePeriod]]( # 10:30-11am
+            TimeRange[HourMinutePeriod, HourMinutePeriod],
+            TIME_RANGE_PATTERN,
+            [_parser_hour_minute, _parser_hour_period],
+        ),
+        Parser[TimeRange[HourMinutePeriod, HourName]]( # 10:30-noon
+            TimeRange[HourMinutePeriod, HourName],
+            TIME_RANGE_PATTERN,
+            [_parser_hour_minute, _parser_hour_name],
+        ),
+        Parser[TimeRange[HourMinutePeriod, HourName]]( # 10am-noon
+            TimeRange[HourMinutePeriod, HourName],
+            TIME_RANGE_PATTERN,
+            [_parser_hour_period, _parser_hour_name],
+        ),
+    ],
+    format_time_range,
+)
+
+@dataclass
+class EventTime:
+    start_date: Optional[datetime.date]
+    start_time: Optional[datetime.time]
+    end_date: Optional[datetime.date]
+    end_time: Optional[datetime.time]
+
+def parse_event_time(text: str, *, today: datetime.date=TODAY) -> EventTime:
     """Early iteration of an event time parser
 
     It has room for improvement, but it works.
@@ -140,40 +317,22 @@ def parse_event_time(text: str, today: datetime.date=TODAY) -> EventTime:
         text: The body of text to search for event times.
     """
     # dates
-    date = None
-    for date_pattern in DATE_PATTERNS:
-        kwargs = date_pattern(text)
-        if kwargs is not None:
-            date = format_date(**kwargs, today=today)
-            break
+    date = DATE_PARSER_CHAIN(text, today=today)
 
-    # time
-    start_time = None
-    end_time = None
-    match = re.search(TIME_RANGE_PATTERN, text, flags=re.IGNORECASE)
-    if match:
-        # time range
-        for time_pattern in TIME_PATTERNS:
-            start_kwargs = time_pattern(match.groups()[0])
-            if start_kwargs is not None:
-                start_time = format_time(**start_kwargs)
-                break
-        for time_pattern in TIME_PATTERNS:
-            end_kwargs = time_pattern(match.groups()[1])
-            if end_kwargs is not None:
-                end_time = format_time(**end_kwargs)
-                break
+    # times
+    time_range = TIME_RANGE_PARSER_CHAIN(text)
+    if time_range is None:
+        start_time = TIME_PARSER_CHAIN(text)
+        end_time = None
     else:
-        # single time
-        for time_pattern in TIME_PATTERNS:
-            start_kwargs = time_pattern(text)
-            if start_kwargs is not None:
-                start_time = format_time(**start_kwargs)
+        start_time, end_time = time_range
 
-    start = (date, start_time)
-    end = (date, end_time)
-
-    return EventTime(start, end)
+    return EventTime(
+        date,
+        start_time,
+        None if end_time is None else date,
+        end_time,
+    )
 
 if __name__ == "__main__":
     text = "It's from 8am-noon on March 17!"
