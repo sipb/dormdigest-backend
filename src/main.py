d47b8c3a1b1507fd5e3e2a6dd17f7a44b0fac1f4
Huy Dai <huydai@mit.edu>
2023-04-12 17:35:27 -0400

    Get FastAPI to use HTTPS

--- src/main.py
+++ src/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+import uvicorn
+import os
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+if __name__ == '__main__':
+    uvicorn.run("main:app",
+                host="localhost",
+                port=8432,
+                reload=True,
+                ssl_keyfile="./src/configs/key.pem",
+                ssl_certfile="./src/configs/cert.pem"
+                )
