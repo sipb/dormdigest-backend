c0545481ad75a68e20f46c54888cc038573c97d1
almonds0166 <almonds@mit.edu>
2023-05-04 17:26:49 -0400

    Nearly ready (#16)
    * add /eat API endpoint
    
    * add html2text converter
    * handle base64 encoded emails
    * reformulate category parser and move to utils/
    
    * Tying things together
    
    * add diagram to readme
    * add token requirement for /eat
    * add test email
    * smarter sender/recipient parsing
    * remove social category
    
    * Update mail_scripts

--- README.md
+++ README.md
@@ -1,7 +1,28 @@
 # dormdigest-backend
+
 Backend API server for dormdigest service
 
+```mermaid
+---
+title: dormdigest system
+---
+flowchart TB
+    Locker[Scripts locker]
+    MySQL[(SIPB\nMySQL db)]
 
-## Documentations
+    subgraph dormdigest.mit.edu
+    Frontend([Frontend]) --> Backend[Backend API]
+    Backend -->|/get_events| Frontend
+    end
+
+    Humans("`**MIT community**`") -->|sends out\ndormspam| Locker
+    Locker -->|/eat| Backend
+    MySQL <--> Backend
+    Frontend --> Humans
+
+    Backend ~~~ MySQL
+```
+
+## Documentation
 
 See [docs](docs) folder.
--- mail_scripts/send_to_backend.py
+++ mail_scripts/send_to_backend.py
@@ -6,15 +6,39 @@ import sys
 import os
 import requests
 
-ENDPOINT = f""
+ENDPOINT = os.getenv("DORMDIGEST_EAT_ENDPOINT")
+WEBHOOK_URL = os.getenv("DORMDIGEST_WEBHOOK")
+TOKEN = os.getenv("DORMDIGEST_TOKEN")
 
 def save_last_email(email, append=False):
   mode = "a" if append else "w"
   with open("last.txt", mode) as f:
     f.write(email)
 
+def pass_to_api(email):
+  if ENDPOINT is None or TOKEN is None:
+    return
+
+  payload = {
+    "email": email,
+    "token": TOKEN,
+  }
+  response = requests.post(ENDPOINT, json=payload)
+  if response.status_code == 200: return
+
+  if WEBHOOK_URL is None: return
+
+  error = {
+    "text": f"{response.status_code}: {response.content}",
+    "attachments": [{
+      "fallback": "(email attached)",
+      "text": email,
+    }]
+  }
+
+  requests.post(WEBHOOK_URL, json=error)
+
 if __name__ == "__main__":
   email = sys.stdin.read()
   save_last_email(email)
-
-
+  pass_to_api(email)
